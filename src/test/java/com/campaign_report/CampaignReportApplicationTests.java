c6e461bde8b59ba77b7644ee575a8d7e0e504f50
akhDev-cloud <a.khomich.dev@gmail.com>
2025-06-04 19:08:02 +0300

    first commit

---
+++ src/test/java/com/campaign_report/CampaignReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campaign_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
